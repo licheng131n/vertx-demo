786a3697d978df4e64d8c8ec51c6d9fd222a874a
licheng131n <licheng131n@126.com>
2016-08-04 17:47:13 +0800

    chong gou

---
+++ src/main/java/com/ifeng/vertx/demo/verticle/ServerVerticle.java
@@ -0,0 +1,47 @@
+package com.ifeng.vertx.demo.verticle;
+
+import as.leap.vertx.rpc.impl.RPCClientOptions;
+import as.leap.vertx.rpc.impl.VertxRPCClient;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+
+/**
+ * Created by licheng1 on 2016/8/4.
+ */
+public abstract class ServerVerticle1 extends AbstractVerticle {
+
+    /**
+     *  绑定RPC服务
+     * @param clazz
+     * @param busAddress
+     * @param <T>
+     * @return
+     */
+    protected <T> T bindService(Class<T> clazz, String busAddress) {
+        RPCClientOptions<T> rpcClientOptions = new RPCClientOptions<T>(vertx)
+                .setBusAddress(busAddress)
+                .setServiceClass(clazz);
+        return new VertxRPCClient<T>(rpcClientOptions).bindService();
+    }
+
+    /**
+     * 启动httpServer监听指定端口
+     * @param port
+     */
+    public void startServer(int port) {
+        HttpServer server = vertx.createHttpServer();
+        Router router = Router.router(vertx);
+        //请求路径过滤及处理handle
+        routes(router);
+        //监听端口等待接收请求
+        server.requestHandler(router::accept).listen(port);
+    }
+
+    /**
+     * 请求路径handle设置
+     * @param router
+     */
+    protected abstract void routes(Router router);
+}
